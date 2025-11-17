aaf21c9732d36c2eebb6f51a846176800c50e5df
Thales Nishida <nishida.tn@gmail.com>
2025-11-17 11:05:47 -0300

    Created TextFieldDefault composable.

---
+++ app/src/main/java/com/thalesnishida/todo/presetention/components/TextFieldDefault.kt
@@ -0,0 +1,46 @@
+package com.thalesnishida.todo.presetention.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun TextFieldDefault(
+    value: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit,
+    singleLine: Boolean = true,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    isError: Boolean = false,
+) {
+    TextField(
+        value = value,
+        modifier = modifier.fillMaxWidth(),
+        singleLine = singleLine,
+        enabled = enabled,
+        isError = isError,
+        readOnly = readOnly,
+        onValueChange = onValueChange,
+        colors = TextFieldDefaults.colors(
+            focusedTextColor = MaterialTheme.colorScheme.primary,
+            unfocusedTextColor = MaterialTheme.colorScheme.primary,
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            disabledContainerColor = Color.Transparent,
+
+            cursorColor = MaterialTheme.colorScheme.primary,
+            focusedIndicatorColor = MaterialTheme.colorScheme.primary,
+            unfocusedIndicatorColor = MaterialTheme.colorScheme.primary,
+            disabledIndicatorColor = MaterialTheme.colorScheme.primary,
+            errorIndicatorColor = Color.Red,
+
+            focusedLabelColor = Color.Transparent,
+            unfocusedLabelColor = Color.Transparent,
+        ),
+    )
+}
