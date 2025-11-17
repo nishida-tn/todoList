43370691880a4bd8b2e7c01b8ea1688f37d69b7a
Thales Nishida <nishida.tn@gmail.com>
2025-11-17 11:06:56 -0300

    Created new Intent to TodoDetails.

--- app/src/main/java/com/thalesnishida/todo/presetention/ui/todolist_details/TodoDetailsIntent.kt
+++ app/src/main/java/com/thalesnishida/todo/presetention/ui/todolist_details/TodoDetailsIntent.kt
@@ -2,4 +2,14 @@ package com.thalesnishida.todo.presetention.ui.todolist_details
 
 sealed class TodoDetailsIntent {
     data class LoadTodoDetails(val todoId: String) : TodoDetailsIntent()
+    data class UpdateTodoDetails(
+        val todoId: String,
+        val title: String,
+        val description: String?,
+        val isCompleted: Boolean
+    ) : TodoDetailsIntent()
+
+    data class FinishTodo(
+        val todoId: String,
+    ) : TodoDetailsIntent()
 }
