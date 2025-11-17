e4e0d42b905834f3b5bb553c040ec9b4a648a362
Thales Nishida <nishida.tn@gmail.com>
2025-11-17 11:02:30 -0300

    Added description params and change the type of createdAt

--- app/src/main/java/com/thalesnishida/todo/domain/usecase/AddTodoUseCase.kt
+++ app/src/main/java/com/thalesnishida/todo/domain/usecase/AddTodoUseCase.kt
@@ -2,19 +2,21 @@ package com.thalesnishida.todo.domain.usecase
 
 import com.thalesnishida.todo.domain.model.Todo
 import com.thalesnishida.todo.domain.repository.TodoRepository
+import com.thalesnishida.todo.extensions.toFormatterString
+import java.time.LocalDateTime
 import java.util.UUID
 import javax.inject.Inject
 
 class AddTodoUseCase @Inject constructor(
     private val todoRepository: TodoRepository
-){
-    suspend operator fun invoke(title: String) {
+) {
+    suspend operator fun invoke(title: String, description: String? = null) {
         val newTodo = Todo(
             id = UUID.randomUUID().toString(),
             title = title,
-            description = null,
+            description = description,
             isCompleted = false,
-            createdAt = System.currentTimeMillis()
+            createdAt = LocalDateTime.now().toFormatterString()
         )
         todoRepository.addTodo(newTodo)
     }
